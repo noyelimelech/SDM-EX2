039363a7ba787f028796dc2b612a6b21ce3f8358
noy elimelech <noyel@mta.ac.il>
2020-09-21 16:43:47 +0300

    Init commit

--- javafx-ui/src/Main.java
+++ javafx-ui/src/Main.java
@@ -1,5 +1,14 @@
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
+import uiComponents.mainComponent.MainComponentController;
+
+
+import java.io.IOException;
+import java.net.URL;
 
 public class Main extends Application {
 
@@ -8,7 +17,28 @@ public class Main extends Application {
     }
 
     @Override
-    public void start(Stage primaryStage) {
+    public void start(Stage primaryStage) throws IOException {
+
+        //CSSFX.start();
+
+        FXMLLoader loader = new FXMLLoader();
+
+        // load main fxml
+        URL mainFXML = getClass().getResource("/uiComponents/mainComponent/mainComponentFXML.fxml");
+        loader.setLocation(mainFXML);
+        ScrollPane root = loader.load();
+
+        // wire up controller
+        MainComponentController histogramController = loader.getController();
+        SDMEgine businessLogic = new SDMEgine(histogramController);
+        histogramController.setPrimaryStage(primaryStage);
+        histogramController.setBusinessLogic(businessLogic);
+
+        // set stage
+        primaryStage.setTitle("Super Duper Market");
+        Scene scene = new Scene(root, 1050, 600);
+        primaryStage.setScene(scene);
+        primaryStage.show();
 
     }
 }
--- javafx-ui/src/uiComponents/mainComponent/MainComponentController.java
+++ javafx-ui/src/uiComponents/mainComponent/MainComponentController.java
@@ -1,4 +1,48 @@
 package uiComponents.mainComponent;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.layout.FlowPane;
+
 public class MainComponentController {
+
+    @FXML private Button loadXMLButton;
+    @FXML private Button showStoresButton;
+    @FXML private Button showItemsButton;
+    @FXML private Button showOrderButton;
+    @FXML private Button oneStoreButton;
+    @FXML private Button buyWiseOrderButton;
+    @FXML private FlowPane dynamicAreaFlowPane;
+
+    @FXML
+    void buyWiseOrderButtonAction() {
+
+
+    }
+
+    @FXML
+    void loadXMLButtonAction() {
+
+    }
+
+    @FXML
+    void oneStoreButtonAction() {
+
+    }
+
+    @FXML
+    void showItemsButtonAction() {
+
+    }
+
+    @FXML
+    void showOrderButtonAction() {
+
+    }
+
+    @FXML
+    void showStoresButtonAction() {
+
+    }
+
 }
