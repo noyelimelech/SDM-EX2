6ae52d7b5bea3d3cc17872f69ec600195a27a6d9
danielsapir <danielsap123@gmail.com>
2020-09-24 14:01:24 +0300

    store FXML is done

--- javafx-ui/src/uiComponents/storeGUI/StoreGUIController.java
+++ javafx-ui/src/uiComponents/storeGUI/StoreGUIController.java
@@ -1,4 +1,17 @@
 package uiComponents.storeGUI;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
+
 public class StoreGUIController {
+
+    @FXML private Label storeIdLabel;
+    @FXML private Label storeNameLabel;
+    @FXML private Label ppkLabel;
+    @FXML private Label amountOfOrder;
+    @FXML private VBox storeItemPlaceHolder;
+    @FXML private VBox ordersPlaceHolder;
+
+
 }
